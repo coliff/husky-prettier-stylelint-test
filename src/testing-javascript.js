330a326efb0d4826f8740e9d6dd20c77c808527f
Christian Oliff <christianoliff@yahoo.com>
2019-06-21 00:50:41 +0900

    Create testing-javascript.js

---
+++ src/testing-javascript.js
@@ -0,0 +1,6 @@
+// Track JS Errors in Google Analytics
+!function(e){var n=function(n){var t=e.document.createElement("a");return t.href=n,t};e.onerror=function(t,o,r,a){var i=n(o).hostname;
+ga("send",{hitType:"event",eventCategory:(i==e.location.hostname||void 0==i||""==i?"":"external ")+"error",eventAction:t,eventLabel:(o+" LINE: "+r+(a?" COLUMN: "+a:"")).trim(),nonInteraction:1})}}(window);
+
+// Track PDF/ZIP Downloads in Google Analytics
+window.onload=function(){var t=document.getElementsByTagName("a");for(i=0;i<t.length;i++)t[i].hostname===location.hostname&&t[i].href.match(/\+(zip|pdf)$/i)&&(t[i].setAttribute("target","_blank"),t[i].onclick=function(){ga("send","event","Downloads","Click",this.getAttribute("href"))})};
